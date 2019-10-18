d129c260de13bb88fe9a7599a8ed81877a7b4a18
James Min <jmin499@gmail.com>
2019-10-18 19:16:45 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -16,7 +16,7 @@ ENFIELD_TENNIS_ACADEMY = [
   "Michael Pemulis"
 ]
 
-def assembled_matrix(arr1, arr2)
+def assembled_matrix
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
   
